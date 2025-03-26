174ea654907c8914a8ee12acb643ea830482d193
Andrea Guerra <a_guerragonzalez@mail.fhsu.edu>
2025-03-26 09:13:32 -0500

    Now?

--- api/quotes/read.php
+++ api/quotes/read.php
@@ -16,18 +16,56 @@ $database = new Database();
 $db = $database->connect();
 $quote = new Quote($db);
 
-$quote->id = isset($_GET['id']) ? $_GET['id'] : die();
+<?php
+header('Content-Type: application/json');
 
-$quote->read_single();
+include_once '../../config/Database.php';
+include_once '../../models/Quote.php';
 
-if($quote->quote != null) {
-    $quote_arr = array(
-        'id' => $quote->id,
-        'quote' => $quote->quote,
-        'author' => $quote->author,
-        'category' => $quote->category
-    );
-    echo json_encode($quote_arr);
-} else {
-    echo json_encode(array('message' => 'No Quote Found'));
+$database = new Database();
+$db = $database->connect();
+
+$quote = new Quote($db);
+
+if(isset($_GET['id'])) {
+    $quote->id = $_GET['id'];
+    $quote->read_single();
+
+    if($quote->quote != null) {
+        $quote_arr = array(
+            'id' => $quote->id,
+            'quote' => $quote->quote,
+            'author' => $quote->author,
+            'category' => $quote->category
+        );
+        echo json_encode($quote_arr);
+    }
+    else {
+        echo json_encode(array('message' => 'No Quote Found'));
+    }
+}
+else {
+    $result = $quote->read();
+    $num = $result->rowCount();
+
+    if($num > 0) {
+        $quotes_arr = array();
+
+        while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            extract($row);
+            
+            $quote_item = array(
+                'id' => $id,
+                'quote' => $quote,
+                'author' => $author,
+                'category' => $category
+            );
+
+            array_push($quotes_arr, $quote_item);
+        }
+
+        echo json_encode($quotes_arr);
+    } else {
+        echo json_encode(array());
+    }
 }
