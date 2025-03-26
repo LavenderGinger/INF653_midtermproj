a3ea59cbe24db92633e0acbf680966aeddb12ddb
Andrea Guerra <a_guerragonzalez@mail.fhsu.edu>
2025-03-26 05:56:39 -0500

    How many commits have I done now??

--- config/Database.php
+++ config/Database.php
@@ -13,7 +13,6 @@
       $this->password = getenv('PASSWORD');
       $this->db_name = getenv('DBNAME');
       $this->host = getenv('HOST');
-      $this->port = getenv('PORT');
     }
 
     public function connect() {
@@ -21,7 +20,7 @@
         return $this->conn;
       }
       else {
-        $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->dbname}";
+        $dsn = "pgsql:host={$this->host};dbname={$this->dbname}";
         try { 
           $this->conn = new PDO($dsn, $this->username, $this->password);
           $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
